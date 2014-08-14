54ea58570901a21dce29d6a860a84d5176859dc3
Jimmy Banchon <jialbanc@espol.edu.ec>
2014-08-13 21:36:55 -0500

    Cambios en las vistas añadidos varios managed Bean

---
+++ MavenPet/MavenPet-web/src/main/java/Beans/CitasManagedBean.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Beans;
+
+import Entities.Citas;
+import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import org.primefaces.event.ScheduleEntryMoveEvent;
+import org.primefaces.event.ScheduleEntryResizeEvent;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.model.DefaultScheduleEvent;
+import org.primefaces.model.DefaultScheduleModel;
+import org.primefaces.model.LazyScheduleModel;
+import org.primefaces.model.ScheduleEvent;
+import org.primefaces.model.ScheduleModel;
+
+/**
+ *
+ * @author Administrador
+ */
+@ManagedBean(name = "citasManagedBean", eager = true)
+@RequestScoped
+public class CitasManagedBean implements Serializable {
+    
+    @EJB
+    private Facades.CitasFacadeLocal citas;
+    private List<Entities.Citas> ltCitas;
+    
+    private ScheduleModel eventModel;
+ 
+    private ScheduleEvent event = new DefaultScheduleEvent();
+ 
+    @PostConstruct
+    public void init() {
+        ltCitas = citas.findAll();
+        for (Citas c : ltCitas){
+            eventModel = new DefaultScheduleModel();
+            //eventModel.addEvent(new DefaultScheduleEvent(c.getIdcitas(), theDayAfter3Pm(), fourDaysLater3pm()));
+        }
+            
+    }
+     
+    public Date getRandomDate(Date base) {
+        Calendar date = Calendar.getInstance();
+        date.setTime(base);
+        date.add(Calendar.DATE, ((int) (Math.random()*30)) + 1);    //set random day of month
+         
+        return date.getTime();
+    }
+    
+    /*public Date getDateFromString(String strdate){
+        Calendar date = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/mm/dd");
+        date.setTime(sdf.parse(strdate));        
+        return date.getTime();
+
+    }*/
+    
+    public Date getInitialDate() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(calendar.get(Calendar.YEAR), Calendar.FEBRUARY, calendar.get(Calendar.DATE), 0, 0, 0);
+         
+        return calendar.getTime();
+    }
+     
+    public ScheduleModel getEventModel() {
+        return eventModel;
+    }
+ 
+    private Calendar today() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DATE), 0, 0, 0);
+ 
+        return calendar;
+    }
+     
+    public ScheduleEvent getEvent() {
+        return event;
+    }
+ 
+    public void setEvent(ScheduleEvent event) {
+        this.event = event;
+    }
+     
+    public void addEvent(ActionEvent actionEvent) {
+        if(event.getId() == null)
+            eventModel.addEvent(event);
+        else
+            eventModel.updateEvent(event);
+         
+        event = new DefaultScheduleEvent();
+    }
+     
+    public void onEventSelect(SelectEvent selectEvent) {
+        event = (ScheduleEvent) selectEvent.getObject();
+    }
+     
+    public void onDateSelect(SelectEvent selectEvent) {
+        event = new DefaultScheduleEvent("", (Date) selectEvent.getObject(), (Date) selectEvent.getObject());
+    }
+     
+    public void onEventMove(ScheduleEntryMoveEvent event) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event moved", "Day delta:" + event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
+         
+        addMessage(message);
+    }
+     
+    public void onEventResize(ScheduleEntryResizeEvent event) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event resized", "Day delta:" + event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
+         
+        addMessage(message);
+    }
+     
+    private void addMessage(FacesMessage message) {
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+}
---
+++ MavenPet/MavenPet-web/src/main/java/Beans/NotificacionesManagedBean.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Beans;
+
+import Entities.Citas;
+import Entities.Notificaciones;
+import Entities.Usuario;
+import Entities.UsuarioHasCitas;
+import Facades.CitasFacadeLocal;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author usuario
+ */
+@ManagedBean(name = "notificacionesManagedBean", eager = true)
+@RequestScoped
+public class NotificacionesManagedBean {
+
+    
+    @EJB
+    private Facades.CitasFacadeLocal citas;
+    private List<Entities.Citas> ltCitas;
+    
+    private List<Entities.Usuario> usuariosfiltrados;
+    
+    public NotificacionesManagedBean() {
+    }
+    
+    @PostConstruct
+    public void init(){
+        ltCitas = citas.findAll();
+    }
+
+    public CitasFacadeLocal getCitas() {
+        return citas;
+    }
+
+    public void setCitas(CitasFacadeLocal citas) {
+        this.citas = citas;
+    }
+
+    public List<Citas> getLtCitas() {
+        return ltCitas;
+    }
+
+    public void setLtCitas(List<Citas> ltCitas) {
+        this.ltCitas = ltCitas;
+    }
+
+    public List<Usuario> getUsuariosfiltrados() {
+        return usuariosfiltrados;
+    }
+
+    public void setUsuariosfiltrados(List<Usuario> usuariosfiltrados) {
+        this.usuariosfiltrados = usuariosfiltrados;
+    }
+    
+}
